9b6cf34962b7db0d6a2a48eed97d3353e29f5ab8
phatcharaphan <phat.pan@gmail.com>
2018-01-27 20:33:15 +0700

    unittest firebase service

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,7 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
-import { AngularFireAuthModule } from 'angularfire2/auth';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
 import { FirebaseService } from '../../services/firebase-service.service';
 import { AngularFireModule } from 'angularfire2';
 import { firebaseConfig } from '../../../environments/firebase.config';
@@ -9,12 +9,14 @@ import { AppRoutingModule } from '../../app-routing/app-routing.module';
 import { CategoryLanguageComponent } from '../category-language/category-language.component';
 import { HomeComponent } from '../home/home.component';
 import { APP_BASE_HREF } from '@angular/common';
+import { Observable } from 'rxjs/Observable';
+import { User, ProfileInfo } from '../../model/user-info';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
-
-  beforeEach(async(() => {
+  let firebaseService: FirebaseService;
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
         LoginComponent,
@@ -27,18 +29,34 @@ describe('LoginComponent', () => {
       ],
       providers: [
         FirebaseService,
-        { provide: APP_BASE_HREF, useValue : '/' }]
+        { provide: APP_BASE_HREF, useValue: '/' }]
     })
       .compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
+    firebaseService = fixture.debugElement.injector.get(FirebaseService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call login with facebook when click facebook', () => {
+    let mockDataUser: User;
+    let mockData: ProfileInfo;
+    mockDataUser = {
+      displayName: 'Phatcharaphan ananpreechakun',
+      email: 'phat.pan@gmail.com'
+    };
+    mockData = {
+      user: mockDataUser
+    };
+    spyOn(firebaseService, 'loginWithFacebook').and.returnValue(Promise.resolve(mockData));
+    component.loginWithFacebook();
+    expect(firebaseService.loginWithFacebook).toHaveBeenCalled();
   });
 });
