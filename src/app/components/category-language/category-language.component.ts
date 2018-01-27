42d44c0cf9b7d7bbcffcfa3fc4b3b8db872a70e7
phatcharaphan <phat.pan@gmail.com>
2018-01-28 00:05:46 +0700

    list category

--- src/app/components/category-language/category-language.component.ts
+++ src/app/components/category-language/category-language.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ModalService } from '../../services/modal.service';
 import { Category } from '../../model/category';
-import { AngularFireDatabase } from 'angularfire2/database';
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-category-language',
@@ -9,15 +10,20 @@ import { AngularFireDatabase } from 'angularfire2/database';
   styleUrls: ['./category-language.component.css']
 })
 export class CategoryLanguageComponent implements OnInit {
-
+  category: Observable<any[]>;
   constructor(private modalService: ModalService, private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.getCategoryLanguage();
   }
 
   add() {
     this.modalService.addCategoryLanguageModal().result.then((data: Category) => {
       this.db.list('category').push(data);
     }, () => { });
+  }
+
+  getCategoryLanguage() {
+    this.category = this.db.list('category').valueChanges();
   }
 }
