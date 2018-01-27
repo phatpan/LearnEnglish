2086089b26c562c6ac90952fd94be632a06066d3
phatcharaphan <phat.pan@gmail.com>
2018-01-28 00:24:20 +0700

    link sentence

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home.component';
 import { LoginComponent } from '../components/login/login.component';
 import { CategoryLanguageComponent } from '../components/category-language/category-language.component';
+import { SentenceComponent } from '../components/sentence/sentence.component';
 
 const routes: Routes = [
+  { path: 'sentence', component: SentenceComponent},
   { path: 'categoryLanguages', component: CategoryLanguageComponent},
   { path: 'home', component: HomeComponent },
   { path: '', component: LoginComponent },
